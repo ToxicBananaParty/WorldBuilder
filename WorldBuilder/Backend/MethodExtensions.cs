0b70261468c91bcf210c61ff2896959a8c4a5a8a
ryandwyer@gyleegames.com <ryan@rydwy.com>
2020-11-28 21:21:40 -0500

    Create solution, import SadConsole, create extension methods

---
+++ WorldBuilder/Backend/MethodExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using SadConsole;
+using Console = SadConsole.Console;
+
+namespace WorldBuilder.Backend
+{
+    public static class MethodExtensions
+    {
+        public static void Print(this Console console, int row, string text)
+        {
+            int col = (Program.width - text.Length) / 2;
+            console.Print(col, row, text);
+        }
+
+        public static void Print(this Console console, int row, string text, Color color)
+        {
+            int col = (Program.width - text.Length) / 2;
+            console.Print(col, row, text, color);
+        }
+    }
+}

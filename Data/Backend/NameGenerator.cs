45f228ee6931384e504ad63d8005491bde390f63
ryandwyer@gyleegames.com <ryan@rydwy.com>
2020-12-06 02:13:46 -0500

    Add human name generator and tester

---
+++ Data/Backend/NameGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WorldBuilder.Data.Backend
+{
+    public class NameGenerator
+    {
+        public class NameList
+        {
+            [JsonProperty("human")]
+            public HumanName humanName;
+
+            public NameList()
+            {
+                humanName = new HumanName();
+            }
+            
+            public class HumanName
+            {
+                public string[] male { get; set; }
+                public string[] female { get; set; }
+                public string[] surname { get; set; }
+
+                public HumanName()
+                {
+                    male = new string[] { };
+                    female = new string[] { };
+                    surname = new string[] { };
+                }
+
+                public string maleName(int index)
+                {
+                    return male[index];
+                }
+
+                public string femaleName(int index)
+                {
+                    return female[index];
+                }
+
+                public string lastName(int index)
+                {
+                    return surname[index];
+                }
+            }
+        }
+
+        private NameList nameList;
+        
+        public NameGenerator()
+        {
+            nameList = new NameList();
+            
+            JsonSerializer serializer = new JsonSerializer();
+            //TODO: Find file
+            using (StreamReader reader = new StreamReader("..//..//..//Data//Backend//names.json"))
+            using (JsonReader jr = new JsonTextReader(reader)) {
+                nameList = serializer.Deserialize<NameList>(jr);
+            }
+        }
+
+        public string GenerateHuman(Gender gender)
+        {
+            //TODO Check gender
+            string firstName =  nameList.humanName.male[Program.RandomInt(nameList.humanName.male.Length)];
+            string lastName = nameList.humanName.lastName(Program.RandomInt(nameList.humanName.surname.Length));
+            return firstName + " " + lastName;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SadConsole;
+using WorldBuilder.Data.Backend;
 
 namespace WorldBuilder
 {
-    class Program
+    static class Program
     {
+        public const int Width = 128;
+        public const int Height = 36;
+
+        private static Random rand;
+        private static NameGenerator nameGenerator;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            rand = new Random(DateTime.Now.Millisecond);
+            nameGenerator = new NameGenerator();
+            
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(Width, Height);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+            SadConsole.Game.OnUpdate += Update;
+
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        public static int RandomInt(int max)
+        {
+            return rand.Next(max);
+        }
+
+        public static int RandomInt(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+
+        private static void Init()
+        {
+            var startingConsole = SadConsole.Global.CurrentScreen;
+            startingConsole.FillWithRandomGarbage();
+            startingConsole.Fill(new Rectangle(3, 3, 27, 5), null, Color.Black, 0, SpriteEffects.None);
+            startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
+        }
+
+        private static void Update(GameTime time)
+        {
+            if (Global.KeyboardState.IsKeyPressed(Keys.F)) {
+                Global.CurrentScreen.Clear();
+                Global.CurrentScreen.Print(6, 5, nameGenerator.GenerateHuman(Gender.CisMale));
+            }
         }
     }
 }

48e35affec2198624469f00440f4fb88ad6abe29
ryandwyer@gyleegames.com <ryan@rydwy.com>
2020-12-06 02:17:45 -0500

    Sanitize method extensions

--- Data/Backend/MethodExtensions.cs
+++ Data/Backend/MethodExtensions.cs
@@ -8,12 +8,20 @@ namespace WorldBuilder.Data.Backend
         public static void Print(this Console console, int row, string text)
         {
             int col = (Program.Width - text.Length) / 2;
+			
+			if(col < 0)
+				col = 0;
+			
             console.Print(col, row, text);
         }
 
         public static void Print(this Console console, int row, string text, Color color)
         {
             int col = (Program.Width - text.Length) / 2;
+			
+			if(col < 0)
+				col = 0;
+			
             console.Print(col, row, text, color);
         }
     }
